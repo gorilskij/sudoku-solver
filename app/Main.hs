4be667d118568bfae0cc027b9c588a39e09a42d9
gorilskij <43991400@users.noreply.github.com>
2020-07-24 10:36:33 +0200

    add a bunch of harder sudokus that I can't yet solve

--- app/Main.hs
+++ app/Main.hs
@@ -3,32 +3,17 @@ module Main where
 import Lib
 
 import Debug.Trace
-
-solvem'' i pss
-    | i `mod` 100 == 0 = trace ("iteration " ++ show i) (solvem' i pss)
-    | otherwise          = solvem' i pss
-
-solvem' :: Int -> [(Sudoku, Sudoku)] -> IO ()
-solvem' _ [] = return ()
-solvem' i ((p, s):pss) = do
-    let solved = solve p
-    if isSolved solved
-        then if solved == s
-                 then solvem'' (i + 1) pss
-                 else error $ "solved but not equal at index " ++ show i
-        else error $ "failed to solve at index " ++ show i
-
-solvem = solvem' 0
+import System.Directory
+import System.FilePath.Posix
+import Data.Function
+import Data.Char
+import Data.List
 
 
-main :: IO ()
-main = do
-    -- pairs <- pairsFromCSV "sudokus/sudoku.csv"
-    -- solvem
-    --     $ take 1000
-    --     $ pairs
-
-    s <- fromFile "sudokus/hardest1.txt"
+--
+solveOne :: FilePath -> IO ()
+solveOne p = do
+    s <- fromFile p
     putStrLn $ showSudoku s
     putStrLn "\n\n\n"
 
@@ -41,3 +26,63 @@ main = do
     putStrLn "\n\n\n"
 
     putStrLn $ showSudoku s'
+--
+
+
+--
+solvePairs'' i pss
+    | i `mod` 100 == 0 = trace ("iteration " ++ show i) (solvePairs' i pss)
+    | otherwise        = solvePairs' i pss
+
+solvePairs' :: Int -> [(Sudoku, Sudoku)] -> IO ()
+solvePairs' _ [] = return ()
+solvePairs' i ((p, s):pss) = do
+    let solved = solve p
+    if isSolved solved
+        then if solved == s
+                 then solvePairs'' (i + 1) pss
+                 else error $ "solved but not equal at index " ++ show i
+        else error $ "failed to solve at index " ++ show i
+
+solvePairs = solvePairs' 0
+--
+
+
+--
+(.:) = (.) . (.)
+
+endsWith :: String -> String -> Bool
+endsWith = on (and .: zipWith (==)) reverse
+
+fileNum :: String -> Int
+fileNum = read
+        . takeWhile isDigit
+        . dropWhile (not . isDigit)
+
+solveFolder :: FilePath -> IO ()
+solveFolder f = do
+    subs <- getDirectoryContents f
+    let subs' = filter (endsWith ".txt") subs
+    let subs'' = sortOn fileNum subs'
+    let subs''' = map (f </>) subs''
+    print subs'''
+    putStrLn "\n\n"
+
+    sequence
+     $ intersperse (putStrLn "\n\n\n===========================================\n\n\n")
+     $ map solveOne subs'''
+
+    return ()
+--
+
+main :: IO ()
+main = do
+    solveFolder "sudokus/hardest"
+
+
+    -- pairs <- pairsFromCSV "sudokus/sudoku.csv"
+    -- solvem
+    --     $ take 1000
+    --     $ pairs
+
+    -- solveOne "sudokus/hardest1.txt"
